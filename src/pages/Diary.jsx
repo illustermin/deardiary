f9b387385e1c2b82c230f29789d0d9c000d01e59
illustermin <2002hongmj@naver.com>
2025-08-21 15:57:23 +0900

    Feat: diary page 구현

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
 
 const Diary = () => {
   const params = useParams();
-
-  return <div>{params.id} 번 일기입니다.</div>;
+  const nav = useNavigate();
+  return (
+    <div>
+      <Header
+        title={"yyyy-mm-dd 기록"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={<Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />}
+      />
+      <Viewer />
+    </div>
+  );
 };
 
 export default Diary;
