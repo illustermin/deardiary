27cbd31c59ae484fcc02952e028401baf0a28732
illustermin <2002hongmj@naver.com>
2025-10-02 23:56:09 +0900

    edit페이지 최종구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -8,7 +8,7 @@ import { DiaryDispatchContext, DiaryStateContext } from "../App";
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
-  const { onDelete } = useContext(DiaryDispatchContext);
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
   const data = useContext(DiaryStateContext);
   const [curDiaryItem, setCurDiaryItem] = useState();
 
@@ -20,11 +20,18 @@ const Edit = () => {
       nav("/", { replace: true });
     }
     setCurDiaryItem(currentDiaryItem);
-  }, [params.id, data]);
+  }, [params.id]);
 
   const onClickDelete = () => {
     if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
-      onDelete(parseInt(params.id));
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      onUpdate(params.id, input.createdDate.getTime(), input.emotionId, input.context);
       nav("/", { replace: true });
     }
   };
@@ -36,7 +43,7 @@ const Edit = () => {
         leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
         rightChild={<Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />}
       />
-      <Editor initData={curDiaryItem} />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
     </div>
   );
 };
